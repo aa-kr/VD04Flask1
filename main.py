b06a81ede12fa7a22f7543b6c59d1b899f3877e7
aa-kr <aa-kr@mail.ru>
2024-08-23 22:21:44 +0300

    add new files

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def company():
+    return render_template("index.html")
+
+
+@app.route("/person/")
+def person():
+    return render_template("blog.html")
+
+@app.route("/contacts/")
+def contacts():
+    return render_template("contacts.html")
+
+
+if __name__ == "__main__":
+    app.run()
